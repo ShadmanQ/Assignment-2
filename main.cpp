99d518521dee5b3361950d3a083544de4fb28a13
Shadman Quazi <shadmanhquazi@gmail.com>
2016-05-13 18:54:33 -0400

    V 0.0.1
    The very first build of this thing. Has the barebones mechanics and some very basic weapons/enemy types. I'm not too sure of the scope of this project, or if there's any scope at all rather.

--- main.cpp
+++ main.cpp
@@ -1,252 +1,145 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
-
-/*
-Name: Shadman Quazi
-Class: CSCI 135
-
-Description: This program uses two files to determine GPA's of a group of students. The first file is of courses, this file is loaded into a large piece of data to be stored and modified later. Same situation with the student file, however before each student gets loaded into a seperate piece of data, a list of classes a particular student is created and linked to that students ID. once both students and courses are loaded into the computer memory, they are used to calculate each students GPA, CS specific GPA, and a percentage of Non Liberal Arts. As these three things are created, they along with the Student ID are put into a new output file.
-
-*/
+#include <stdlib.h>
 
 using namespace std;
+struct player//player information, currently hard coded into the source, in future builds will hopefully be loaded from files
+{
+double HP = 200;
+double END = 200;
+double STR = 30;
 
-struct Class
-{
- int term;
- string subject;
- string catalog;
- string letGrade;
- double numGrade;
- Class* next ;
 };
-typedef Class* ClassList;
-struct Student
+struct enemy//this and the player struct are fairly simple, I will add more attributes and stats soon
 {
- string ID;
- ClassList ClassesTaken; // a linked list of Class objects
+string enemy_name;
+double HP;
+double END;
+double STR;
 };
-struct Course
+struct weapon//same with this one
 {
- string subject;
- string catalog;
- double hours;
- string DR; // designation requirement
+string name;
+double PWR;
+double FTG;
 };
 
-void LoadCourseData(ifstream& x, vector <Course>& y);
-void LoadStudentData (ifstream& IN, vector<Student>& sVec);
-void add_class(int tm, string sub, string cat, string lGrade, double numGrades, ClassList& headp);
-void create_student(Student& temp, string& object, int& t, string& s, string& c, string& LG, double& NG);
-int validate(string x, vector<Student>& sVec);
-void sort_Vec(vector <Student>& sVec);
-void out_put(vector <Student>& x, vector <Course>& y, ofstream& OUT);
-void Traverse(ClassList& TheList);
-void Calc_GPA(double& Grades, ClassList ThisList, vector <Course> CV);
-void CSCI_GPA_Calc(double& Grades, ClassList ThisList, vector <Course> CV);
-void NLA_Calc(double& ptage, ClassList thislist, vector <Course> y);
+void Fight_Display( string n, double x, double y)//displays the health of each party involved in the fight
+{
+    cout << "The "<<(n)<<" has "<< (x)<<"HP. You have"<<(y)<<"Hp"<<endl;
+}
+void load_weapon(weapon& x, string y, double s, double f)
+{
+    x.name = y;
+    x.PWR = s;
+    x.FTG = f;
+}
 
+void choose_weapon(weapon& w)
+{
+cout << "Please a choose a weapon (enter a number between (0-3)"<<endl;
+int input;
+char confirm;
+cin>>input;
+if (input == 0)
+{
+load_weapon (w, "Fists", 5, 5);
+}
+
+if (input == 1)
+{
+load_weapon (w, "Axe", 20, 25);
+}
+
+if (input == 2)
+{
+load_weapon (w, "Sword", 15, 15);//each weapon data also hard coded, like with the player info I hope to load this from file soon
+}
+
+if (input == 3)
+{
+load_weapon (w, "Staff", 10, 5);
+}
+cout<<"Is this the weapon you want? (type y or n)"<<endl;
+cin >> confirm;
+
+if (confirm == 'n')
+{
+    choose_weapon(w);
+}
+}
+
+void load_enemy(enemy& x, string n, int eh, int ee, int es)
+{
+    x.enemy_name = n;
+    x.HP = eh;
+    x.END = ee;
+    x.STR = es;
+}
+void choose_eneme(enemy& thing)
+{
+    int enemyinput;
+    char confirm;
+    cout << "Please choose an enemy (enter a number between 1-3)"<<endl;
+    cin >> enemyinput;
+{
+if (enemyinput == 1)
+{
+    load_enemy(thing, "Ogre", 500, 200, 150);
+}
+if (enemyinput == 2)
+{
+    load_enemy(thing, "Wolf",250,400,75);
+}
+if (enemyinput == 3)
+{
+    load_enemy(thing, "Chicken", 5000, 5000, 5000);
+}
+
+}
+cout << "You have chosen "<<thing.enemy_name<<". Is this the enemy you want? (type y or n)"<<endl;
+cin >> confirm;
+if (confirm == 'n')
+{
+    choose_eneme(thing);
+}
+}
 int main()
 {
+int input, enemyinput;
+double damage = 1.2;//the damage value will not be stuck at 1.2, lol. I will use a random number generator for this
+weapon myWeapon;
+enemy Theenemy;
+player Me;
 
-ifstream HCInput, SDInput;
-ofstream SDoutput;
-vector <Course> CourseVector;
-vector <Student> StudentVector;
-LoadCourseData(HCInput, CourseVector);
-LoadStudentData(SDInput, StudentVector);
-sort_Vec(StudentVector);
-cout<<StudentVector.size();
-out_put(StudentVector,CourseVector, SDoutput);
-HCInput.close();
-SDInput.close();
-SDoutput.close();
+choose_weapon(myWeapon);
+choose_eneme(Theenemy);
 
- return 0;
+double pDamage;
+double eDamage;
+while( Theenemy.HP >0 && Me.HP >0)//the "Battle block" so to speak. Uses simple math calculations to determine damage done and stamina lost to each party
+{
+Fight_Display(Theenemy.enemy_name, Theenemy.HP, Me.HP);
+pDamage = ((Me.STR + myWeapon.PWR)*(1.00+(Me.END/100)));
+eDamage = (Theenemy.STR *(1.00+Theenemy.END/100));
+Theenemy.HP -= pDamage;
+Me.HP -= eDamage;
+Me.END -= myWeapon.FTG;
+Theenemy.END -= Theenemy.STR/35;
+cout << Me.HP<<"\t"<<Me.END<<endl;
+cout << Theenemy.HP<<"\t"<<Theenemy.END<<endl;
+}
+if (Me.HP<=0.0)//the bad ending
+{
+    cout<<"You died. Game over! :("<<endl;
 }
 
-void LoadCourseData(ifstream& x, vector <Course>& y)//Load of the course data vector via file input
+if (Theenemy.HP <=0.0)//the good ending
 {
-	x.open("HunterCourses.tsv");
-	Course tempCourse;
-	while(x >> tempCourse.subject>>tempCourse.catalog>>tempCourse.hours>>tempCourse.DR)//allows a full student to be made
-	{
-		 y.push_back(tempCourse);
-	}
-
+    cout<<"The "<<Theenemy.enemy_name<<" died. You win!"<<endl;
 }
 
-
-int validate(string x, vector<Student>& sVec)//to check if the current student is present in the student vector
-{
-	int i;
-	for (int i = 0; i <= sVec.size(); i++)
-	{
-		if (sVec[i].ID == x)
-		{
-            		return i;//returns the specific index of the student in the student vector, allowing add_class to act on this specific index
-		}
-	}
-	return -8675309;//return value for if the student isn't present in Student Vector, allowing the create_student function to initialize in main block
-}
-
-void add_class (ClassList& thingamajig, int tm, string id, string sub, string cat, string lg, double ng)//creates a node to be linked with a previously existing list dependent on the studend
-{
-ClassList NewClass;
-NewClass = new Class;//a new node, with garbage to be filled
-NewClass-> term= tm;
-NewClass->subject = sub;
-NewClass->catalog = cat;
-NewClass->letGrade = lg;
-NewClass->numGrade = ng;
-NewClass->next = thingamajig;
-thingamajig = NewClass;
-}
-
-
-void create_student(vector <Student>& studVec, Student& aStud, ClassList& aList, int& tm, string& id, string& sub, string& cat, string& lg, double& ng)//uses objects that were streamed in by the LoadStudentData function
-{
-	aStud.ID = id;
-	ClassList firstClass;
-	firstClass = new Class;//creation of new Class Node to be filled
-	Class* top = new Class;
-	firstClass->term = tm;
-	firstClass->subject = sub;
-	firstClass->catalog = cat;
-	firstClass->letGrade = lg;
-	firstClass->numGrade = ng;
-	firstClass->next = NULL;//this block effectively creates a "data filled" head node for an eventual linked list, hence the NULL.
-	aList=firstClass;
-	studVec.push_back(aStud);
-}
-
-void LoadStudentData (ifstream& IN, vector<Student>& sVec)//to create the Student Vector via file input and a student vector present in the main block
-{
-IN.open("StudentData.tsv");
-int stud_term;
-string stud_ID, stud_subject, stud_catalog, stud_letGrade;
-double stud_numGrade;
-Student first_stud;
-IN>>stud_ID>>stud_term>>stud_subject>>stud_catalog>>stud_letGrade>>stud_numGrade;
-create_student(sVec, first_stud, first_stud.ClassesTaken, stud_term, stud_ID, stud_subject, stud_catalog, stud_letGrade, stud_numGrade);//used to put an initial element in the Vector
-while(IN>>stud_ID>>stud_term>>stud_subject>>stud_catalog>>stud_letGrade>>stud_numGrade)
-	{
-		int check = validate(stud_ID, sVec);
-		if (check < 0)//i.e. the student doesn't exist in the vector
-		{
-			Student temp_dude;
-			create_student(sVec, temp_dude, temp_dude.ClassesTaken, stud_term, stud_ID, stud_subject, stud_catalog, stud_letGrade, stud_numGrade);
-		}
-		if (check >= 0)//i,.e. the student does exist in the vector
-		{
-			add_class(sVec[check].ClassesTaken, stud_term, stud_ID, stud_subject, stud_catalog, stud_letGrade, stud_numGrade);
-		}
-
-	}
-}
-
-void sort_Vec(vector <Student>& sVec)//to put the vector in numerical order by EMPL ID
-{
-    for(int i=0; i<sVec.size(); i++)
-	{
-	        for(int j=i; j<sVec.size(); j++)
-		{
-	            if(sVec[i].ID>sVec[j].ID)
-			{
-	                Student tempStudent;
-	                tempStudent = sVec[i];
-	                sVec[i]=sVec[j];
-	                sVec[j]=tempStudent;
-			}
-		}
-	}
-}
-
-
-void CSCI_GPA_Calc(double& CS_Grades, ClassList ThisList, vector <Course> CV)//creates a CS specific GPA via pass by reference of a temp CSCI_GPA in the main block
-{
-double hours=0.0;
-double creds = 0.0;
-double courseWeight = 0.0;
-Class* traverse = ThisList;//pointer used to access the data of each node in ThisList
-while(traverse!=NULL)
-{
-	for (int j = 0; j <CV.size(); j++)
-		{
-			if ((traverse->subject == "CSCI") &&(traverse->subject == CV[j].subject) && (traverse->catalog == CV[j].catalog) && (traverse->numGrade != -1))
-				{
-				courseWeight=((traverse->numGrade)*CV[j].hours);
-				creds +=courseWeight;
-				hours +=CV[j].hours;
-				}
-		}//this loop is used to find the total number CS_credits earned by a stuend and the number hours this student took part in
-traverse=traverse->next;//to move to the next node
-}
-CS_Grades =creds/hours;//where the GPA is actually calculated
-}
-
-void Calc_GPA(double& Grades, ClassList ThisList, vector <Course> CV)//creates an overall GPA via pass-by reference of a temp GPA in the main block
-{
-double hours=0.0;
-double creds = 0.0;
-double courseWeight = 0.0;
-Class* traverse = ThisList;//pointer used to access the data of each node
-while(traverse!=NULL)
-{
-	for (int j = 0; j <CV.size(); j++)
-		{
-			if ((traverse->subject == CV[j].subject) && (traverse->catalog == CV[j].catalog) && (traverse->numGrade != -1))
-				{
-				courseWeight=((traverse->numGrade)*(CV[j].hours));
-				creds +=courseWeight;
-				hours +=CV[j].hours;
-				}
-		}//this loop is used to find the total number credits earned by a stuend and the number hours this student took part in
-traverse=traverse->next;// to move to the next node
-}
-Grades =creds/hours;//where the GPA is actually calculated
-}
-
-
-void NLA_Calc(double& ptage, ClassList thisList, vector <Course> y)
-{
-
-double tot_hours = 0.0;
-double NL_hours =0.0;
-Class* traverse = thisList;
-while(traverse!=NULL)
-	{
-		for (int  i = 0; i<y.size(); i++)
-		{
-			if((y[i].subject == traverse->subject) && (y[i].catalog) == (traverse->catalog))
-				{
-					tot_hours+=y[i].hours;
-					if (y[i].DR == "RNL" || y[i].DR == "MNL" || y[i].DR == "GNL")
-					{
-						NL_hours+=y[i].hours;
-					}
-				}
-
-		}
-		traverse = traverse->next;
-	}
-	ptage = (NL_hours/tot_hours)*100;
-
-}
-void out_put(vector <Student>& x, vector <Course>& y, ofstream& OUT)//final output function
-{
-
-OUT.open("StudentSummary.tsv");//file output creation
-double GPA = 0.0,  CSCI_GPA = 0.0, NLA_percentage = 0.0;//temp variables to be manipulated by the void functions below
-for (int i=0; i<x.size();i++)
-{
-
-Calc_GPA(GPA, x[i].ClassesTaken, y);
-CSCI_GPA_Calc(CSCI_GPA, x[i].ClassesTaken, y);
-NLA_Calc(NLA_percentage, x[i].ClassesTaken, y);
-OUT<<x[i].ID<<"\t"<<GPA<<"\t"<<CSCI_GPA<<"\t"<<NLA_percentage<<"%"<<endl;
-}
-
+system("PAUSE");
+return 0;
 }
